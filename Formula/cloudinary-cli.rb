2bd40cfe10630c756188e73d2079b3cb262fd4f4
anjuls <anjuls@users.noreply.github.com>
2026-09-24 10:00:54 +0530

    Remove license field; repo has no LICENSE file yet

--- Formula/cloudinary-cli.rb
+++ Formula/cloudinary-cli.rb
@@ -3,7 +3,6 @@ class CloudinaryCli < Formula
   homepage "https://github.com/anjuls/cloudinary-cli"
   url "https://github.com/anjuls/cloudinary-cli/archive/refs/tags/v0.1.0.tar.gz"
   sha256 "b84ffbb3f336a2a716440d6df052a4806869a7014add93173b6c9a476765f96a"
-  license "MIT"
   head "https://github.com/anjuls/cloudinary-cli.git", branch: "main"
 
   depends_on "go" => :build
